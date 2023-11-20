d43774dd2823aae379ca0182bae744ce257c43ab
ikramiste <ikrame.saadi@gmail.com>
2023-11-20 18:19:50 +0100

    deleting comments

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 ////////////////////////////////////F1////////////////////////////////////
-//CLICK & COUNT
 
 const footer = document.querySelector("footer");
 let countMyClicks = 0;
@@ -9,17 +8,8 @@ footer.addEventListener("click", function () {
   console.log("Tu critiques mais t'as cliqué " + countMyClicks + " fois !");
 });
 
-/* Mais dis donc Jamy, que fais-tu exactement ? 
-
----> On selectionne le footer.
----> On initie le compteur de clics.
----> On incrémente à chaque event (click).
----> On affiche le résultat en console (console.log).
-
-*/
-
 ////////////////////////////////////F2////////////////////////////////////
-//TOGGLE MENU
+
 
 const menuBtn = document.querySelector(".navbar-toggler");
 const navHead = document.getElementById("navbarHeader");
@@ -28,16 +18,8 @@ menuBtn.addEventListener("click", function () {
   navHead.classList.toggle("collapse");
 });
 
-/* Mais dis donc Jamy, que fait .toggle exactement ?
-
-navHead a la classe ("collapse") ? ---> classe supprimée (= menu caché)
-navHead n'a pas la classe ("collapse") ? ---> classe ajoutée (= menu affiché)
-
-----> Chaque fois que le bouton est cliqué, la fonction est déclenchée.
-*/
 
 ////////////////////////////////////F3////////////////////////////////////
-//REDit BUTTON
 
 const firstCard = document.querySelectorAll(".card")[0];
 
